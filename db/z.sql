7b4592c29b29c3476ba294e0900c63da052161c2
Daniel Reuter <dreuter@kth.se>
2018-11-22 01:35:26 +0100

    database update

--- db/z.sql
+++ db/z.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `z_ots_comunication` (
+CREATE TABLE IF NOT EXISTS `z_ots_comunication` (
   `id` int(11) NOT NULL auto_increment,
   `name` varchar(255) NOT NULL,
   `type` varchar(255) NOT NULL,
@@ -13,3 +13,18 @@ CREATE TABLE `z_ots_comunication` (
   `delete_it` int(2) NOT NULL default '1',
   PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `z_ots_guildcomunication` (
+  `id` INT(11) NOT NULL,
+  `name` VARCHAR(255) NOT NULL,
+  `type` VARCHAR(255) NOT NULL,
+  `action` VARCHAR(255) NOT NULL,
+  `param1` VARCHAR(255) NOT NULL,
+  `param2` VARCHAR(255) NOT NULL,
+  `param3` VARCHAR(255) NOT NULL,
+  `param4` VARCHAR(255) NOT NULL,
+  `param5` VARCHAR(255) NOT NULL,
+  `param6` VARCHAR(255) NOT NULL,
+  `param7` VARCHAR(255) NOT NULL,
+  `delete_it` INT(2) NOT NULL DEFAULT '1'
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
